c81faed6a719d691d5cc6707ed365c29404a543b
mmuyskens <mmuyskens@gmail.com>
2019-11-11 23:20:49 -0800

    Create lulz.js

---
+++ lulz.js
@@ -0,0 +1,8 @@
+var numba = 1;
+var doshit = function(){
+    document.querySelector('[aria-label="Like"]').click();
+    console.log(numba);
+	numba++;
+    setTimeout(doshit, 100);
+};
+setTimeout(doshit, 100);
