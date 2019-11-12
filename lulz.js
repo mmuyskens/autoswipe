02e3864e0acb175a20e3cba0680e2198dec4853c
mmuyskens <mmuyskens@gmail.com>
2019-11-11 23:24:41 -0800

    Update lulz.js

--- lulz.js
+++ lulz.js
@@ -2,7 +2,7 @@ var numba = 1;
 var doshit = function(){
     document.querySelector('[aria-label="Like"]').click();
     console.log(numba);
-	numba++;
+    numba++;
     setTimeout(doshit, 100);
 };
 setTimeout(doshit, 100);
